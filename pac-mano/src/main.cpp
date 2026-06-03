67706bc9418a1387750188fbea8e400b76ed5708
mathe <mathe@GILBERTOAMARAL>
2026-06-03 15:01:14 -0300

    Adicionar arquivos de projeto.

---
+++ pac-mano/src/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Selecione o algoritmo a ser utilizado: " << endl;
+	cout << endl << "[1] A*" << endl;
+	cout << endl << "[2] Poda Alfa-Beta" << endl;
+	cout << endl << endl;
+
+	int s = 0;
+	while (s != 1 && s != 2) {
+		cin >> s;
+		if (s == 1) {
+			cout << "Exec. A*";
+		}
+		else if (s == 2) {
+			cout << "Exec. Poda Alfa-Beta";
+		}
+	}
+	
+	return 0;
+}
